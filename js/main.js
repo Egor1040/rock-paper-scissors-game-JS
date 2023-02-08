a2c5b449fe9d16e0ae140e37a33d8eb1634400ca
Igor <igor.motriy3@gmail.com>
2023-02-08 17:36:37 +0200

    game created v1.0

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let player1 = prompt('Гра починається!Гравець #1 Введіть свій варіант з даних: Камінь, Ножиці, Папір');
+let player2 = prompt('Гра починається!Гравець #2 Введіть свій варіант з даних: Камінь, Ножиці, Папір');
+
+let wordGame1 = 'Камінь';
+let wordGame2 = 'Ножиці';
+let wordGame3 = 'Папір';
+
+if (player1 == wordGame1 && player2 == wordGame1 || player1 == wordGame2 && player2 == wordGame2 || player1 == wordGame3 && player2 == wordGame3) {
+    alert ('Ніхто не переміг, нічия');
+} else if (player1 == wordGame1 && player2 == wordGame2 || player1 == wordGame1 && player2 == wordGame3) {
+    alert ('Переміг гравець #1');
+} if (player1 == wordGame2 && player2 == wordGame3) {
+    alert ('Переміг гравець #1');
+} if (player1 == wordGame3 && player2 == wordGame2 || player1 == wordGame3 && player2 == wordGame1 || player1 == wordGame2 && player2 == wordGame1 ) {
+    alert ('Переміг гравець #2');
+} else {
+    alert ('Ви ввели невірно ігрове слово,повторіть спробу = )');
+}
