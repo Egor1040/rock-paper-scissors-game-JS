d80f29befc3ce45aae03a37c2f7bbbb041aaf074
Igor <igor.motriy3@gmail.com>
2023-02-10 13:58:43 +0200

    rename wordGame

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 let player1 = prompt('Гра починається!Гравець #1 Введіть свій варіант з даних: Камінь, Ножиці, Папір');
 let player2 = prompt('Гра починається!Гравець #2 Введіть свій варіант з даних: Камінь, Ножиці, Папір');
 
-let wordGame1 = 'Камінь';
-let wordGame2 = 'Ножиці';
-let wordGame3 = 'Папір';
+let wordStone = 'Камінь';
+let wordScissor = 'Ножиці';
+let wordPaper = 'Папір';
 
 // Ігрова Логіка
 
-// if (player1 == wordGame1 && player2 == wordGame1 || player1 == wordGame2 && player2 == wordGame2 || player1 == wordGame3 && player2 == wordGame3) {
+// if (player1 == wordStone && player2 == wordStone || player1 == wordScissor && player2 == wordScissor || player1 == wordPaper && player2 == wordPaper) {
 //     alert ('Ніхто не переміг, нічия');
-// } else if (player1 == wordGame1 && player2 == wordGame2 || player1 == wordGame2 && player2 == wordGame3 || player1 == wordGame3 && player2 == wordGame1) {
+// } else if (player1 == wordStone && player2 == wordScissor || player1 == wordScissor && player2 == wordPaper || player1 == wordPaper && player2 == wordStone) {
 //     alert ('Переміг гравець #1');
-// } else if (player1 == wordGame1 && player2 == wordGame3 || player1 == wordGame3 && player2 == wordGame2 || player1 == wordGame2 && player2 == wordGame1 ) {
+// } else if (player1 == wordStone && player2 == wordPaper || player1 == wordPaper && player2 == wordScissor || player1 == wordScissor && player2 == wordStone ) {
 //     alert ('Переміг гравець #2');
 // } else {
 //     alert ('Ви ввели невірно ігрове слово,повторіть спробу = )');
 // }
 
 // Гра фінальна версія
-if (player1 == wordGame1 && player2 == wordGame1) {
+if (player1 == wordStone && player2 == wordStone) {
     alert ('Нічия! Обидва гравці обрали Камінь');
-} else if (player1 == wordGame2 && player2 == wordGame2) {
+} else if (player1 == wordScissor && player2 == wordScissor) {
     alert ('Нічия! Обидва гравці обрали Ножиці');
-} else if (player1 == wordGame3 && player2 == wordGame3) {
+} else if (player1 == wordPaper && player2 == wordPaper) {
     alert ('Нічия! Обидва гравці обрали Папір');
-} else if (player1 == wordGame1 && player2 == wordGame2) {
+} else if (player1 == wordStone && player2 == wordScissor) {
     alert ('Гравець #1 Переміг!!! Гравці Обрали #1 Камінь - #2 Ножиці');
-} else if (player1 == wordGame2 && player2 == wordGame3) {
+} else if (player1 == wordScissor && player2 == wordPaper) {
     alert ('Гравець #1 Переміг!!! Гравці Обрали #1 Ножиці - #2 Папір');
-} else if (player1 == wordGame3 && player2 == wordGame1) {
+} else if (player1 == wordPaper && player2 == wordStone) {
     alert ('Гравець #1 Переміг!!! Гравці Обрали #1 Папір - #2 Камінь');
-} else if (player1 == wordGame1 && player2 == wordGame3) {
+} else if (player1 == wordStone && player2 == wordPaper) {
     alert ('Гравець #2 Переміг!!! Гравці Обрали #1 Камінь - #2 Папір');
-} else if (player1 == wordGame3 && player2 == wordGame2) {
+} else if (player1 == wordPaper && player2 == wordScissor) {
     alert ('Гравець #2 Переміг!!! Гравці Обрали #1 Папір - #2 Ножиці');
-} else if (player1 == wordGame2 && player2 == wordGame1) {
+} else if (player1 == wordScissor && player2 == wordStone) {
     alert ('Гравець #2 Переміг!!! Гравці Обрали #1 Ножиці - #2 Камінь');
 } else {
     alert ('Упс,хтось з гравців некоректно ввів ігрове слово,повторіть спробу =)');
